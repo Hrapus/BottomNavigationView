c71a9ed47913d2ab41b3df938285cc4797d4dccb
Hrapus <hrapus@gmail.com>
2022-09-27 21:11:45 -0400

    Initial commit

--- app/src/main/java/com/example/bottomnavigationview/MainActivity.kt
+++ app/src/main/java/com/example/bottomnavigationview/MainActivity.kt
@@ -2,10 +2,34 @@ package com.example.bottomnavigationview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.bottomnavigationview.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var bindind: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        bindind = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(bindind.root)
+        bindind.bNav.selectedItemId = R.id.item2
+
+        bindind.bNav.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.item1 -> {
+                    Toast.makeText(this, "Item 1", Toast.LENGTH_SHORT).show()
+                }
+                R.id.item2 -> {
+                    Toast.makeText(this, "Item 2", Toast.LENGTH_SHORT).show()
+                }
+                R.id.item3 -> {
+                    Toast.makeText(this, "Item 3", Toast.LENGTH_SHORT).show()
+                }
+                R.id.item4 -> {
+                    Toast.makeText(this, "Item 4", Toast.LENGTH_SHORT).show()
+                }
+            }
+            return@setOnItemSelectedListener true
+        }
     }
 }
